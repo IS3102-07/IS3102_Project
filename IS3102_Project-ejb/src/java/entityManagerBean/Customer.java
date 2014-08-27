2fef24bc558663a11ce5bf711326c73b85d61f4d
Yuan Guang <leeyuanguang.lyg@gmail.com>
2014-08-27 17:23:51 +0800

    removed the test1 test2 test3 & //liangyang comment added in to test the github previously

--- IS3102_Project-ejb/src/java/entityManagerBean/Customer.java
+++ IS3102_Project-ejb/src/java/entityManagerBean/Customer.java
@@ -21,11 +21,7 @@ public class Customer implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String test;
-    private String test2;
-    private String test3;
-    
+    private Long id;   
     
  
     
