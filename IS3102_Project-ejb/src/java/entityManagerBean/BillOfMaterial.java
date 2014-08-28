923ef5fdc35afda898a42142fe4c2c0188cb419f
zhengxiaodong1991 <a0099518@nus.edu.sg>
2014-08-28 10:38:09 +0800

    Testing Add slash///

--- IS3102_Project-ejb/src/java/entityManagerBean/BillOfMaterial.java
+++ IS3102_Project-ejb/src/java/entityManagerBean/BillOfMaterial.java
@@ -17,6 +17,7 @@ import javax.persistence.Id;
  * @author Administrator awdawdawd
  */
 // added 1
+/////
 @Entity
 public class BillOfMaterial implements Serializable {
     private static final long serialVersionUID = 1L;
