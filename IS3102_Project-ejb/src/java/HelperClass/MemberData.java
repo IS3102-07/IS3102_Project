7f54441dcd0f9bc165e2c47a7a3ffb0b28ff11f3
Yuan Guang <leeyuanguang.lyg@gmail.com>
2014-08-30 02:03:52 +0800

    Added MemberData helper class

---
+++ IS3102_Project-ejb/src/java/HelperClass/MemberData.java
@@ -0,0 +1,175 @@
+package HelperClass;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class MemberData implements Serializable {
+    private long id;
+    private String name;
+    private String address;
+    private Date DOB;
+    private String email;
+    private Integer phone;
+    private String country;
+    private String city;
+    private Integer zipCode;
+    private String username;
+    private Integer points;
+
+    /* Creates a new instance  */
+    public void create(Long id, String name, String address, Date DOB, String email, Integer phone, String country, String city, Integer zipCode, String username, Integer points) {
+        this.setId(id);
+        this.setName(name);
+        this.setAddress(address);
+        this.setDOB(DOB);
+        this.setEmail(email);
+        this.setPhone(phone);
+        this.setCountry(country);
+        this.setCity(city);
+        this.setZipCode(zipCode);
+        this.setUsername(username);
+        this.setZipCode(zipCode);
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public long getId() {
+        return id;
+    }
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.setPhone(phone);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the DOB
+     */
+    public Date getDOB() {
+        return DOB;
+    }
+
+    /**
+     * @param DOB the DOB to set
+     */
+    public void setDOB(Date DOB) {
+        this.DOB = DOB;
+    }
+
+    /**
+     * @param phone the phone to set
+     */
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * @return the country
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     * @param country the country to set
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    /**
+     * @return the city
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * @param city the city to set
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * @return the zipCode
+     */
+    public Integer getZipCode() {
+        return zipCode;
+    }
+
+    /**
+     * @param zipCode the zipCode to set
+     */
+    public void setZipCode(Integer zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the points
+     */
+    public Integer getPoints() {
+        return points;
+    }
+
+    /**
+     * @param points the points to set
+     */
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+
+}
