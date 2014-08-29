9ea7e5d1f4f011b09cc0f0fc99144d674fe942ef
Neo Wei <neowei03@gmail.com>
2014-08-29 17:42:20 +0800

    Merge origin/master
    Conflicts:
    	IS3102_Project-ejb/src/java/entityManagerBean/SalesRecord.java

--- IS3102_Project-ejb/src/java/em/StoreEntity.java
+++ IS3102_Project-ejb/src/java/em/StoreEntity.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package em;
 
 import java.io.Serializable;
@@ -12,10 +6,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-/**
- *
- * @author Administrator
- */
 @Entity
 public class StoreEntity implements Serializable {
     private static final long serialVersionUID = 1L;
