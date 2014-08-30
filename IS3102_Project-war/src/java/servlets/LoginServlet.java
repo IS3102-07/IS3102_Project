b02b916ee57173379445ec8b3f775702fe81749f
Neo Wei <neowei03@gmail.com>
2014-08-30 12:49:29 +0800

    Updated index.html and added servlets

---
+++ IS3102_Project-war/src/java/servlets/LoginServlet.java
@@ -0,0 +1,84 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginServlet extends HttpServlet {
+
+    private String result;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        try {
+            HttpSession session = request.getSession();
+            session.removeAttribute("member");
+
+//            List<String> memberDetails = shsmbr.getMember(passportNumber, password);
+//            if (memberDetails == null) {
+//                out.println("1");
+//                result = "Login fail. Please try again.";
+//                response.sendRedirect("index.jsp?errMsg=" + result);
+//            } else {
+//                memberDetails.get(0);
+//                out.println("2");
+//                session.setAttribute("member", memberDetails);
+//                response.sendRedirect("index.jsp");
+//            }
+
+        } catch (Exception ex) {
+            out.println("3");
+            result = "Login fail. Please try again.";
+            response.sendRedirect("index.jsp?errMsg=" + result);
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
