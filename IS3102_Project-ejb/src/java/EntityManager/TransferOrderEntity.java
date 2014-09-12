f92be7eb7040b2ce0e755ded4eb1c23327df1af2
Jason Sim <simmengkiat@gmail.com>
2014-09-12 14:53:39 +0800

    Added StorageBinEntity and TransferOrderEntity Modified WarehouseEntity to link with StorageBinEntity Added the methods for ManufacturingWarehouseManagementBean

---
+++ IS3102_Project-ejb/src/java/EntityManager/TransferOrderEntity.java
@@ -0,0 +1,106 @@
+package EntityManager;
+
+import static com.sun.javafx.accessible.utils.ControlTypeIds.CALENDAR;
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+
+@Entity
+public class TransferOrderEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @OneToMany
+    private List<LineItemEntity> lineItems;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dateTransferred;
+    private Object origin;
+    private Object target;
+
+    public TransferOrderEntity(List<LineItemEntity> lineItems, Object origin, Object target) {
+        this.lineItems = lineItems;
+        this.origin = origin;
+        this.target = target;
+        this.dateTransferred = Calendar.getInstance().getTime();
+//        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+//        Calendar cal = Calendar.getInstance();
+//        dateFormat.format(cal.getTime());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<LineItemEntity> getItems() {
+        return lineItems;
+    }
+
+    public void setItems(List<LineItemEntity> items) {
+        this.lineItems = items;
+    }
+
+    public Date getDateTransferred() {
+        return dateTransferred;
+    }
+
+    public void setDateTransferred(Date dateTransferred) {
+        this.dateTransferred = dateTransferred;
+    }
+
+    public Object getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(Object origin) {
+        this.origin = origin;
+    }
+
+    public Object getTarget() {
+        return target;
+    }
+
+    public void setTarget(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof TransferOrderEntity)) {
+            return false;
+        }
+        TransferOrderEntity other = (TransferOrderEntity) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "EntityManager.TransferOrderEntity[ id=" + id + " ]";
+    }
+
+}
--- IS3102_Project-ejb/src/java/SCM/ManufacturingWarehouseManagement/ManufacturingWarehouseManagementBean.java
+++ IS3102_Project-ejb/src/java/SCM/ManufacturingWarehouseManagement/ManufacturingWarehouseManagementBean.java
@@ -1,20 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package SCM.ManufacturingWarehouseManagement;
 
+import EntityManager.ItemEntity;
+import EntityManager.LineItemEntity;
+import EntityManager.StorageBinEntity;
+import EntityManager.TransferOrderEntity;
+import java.util.List;
 import javax.ejb.Stateless;
 
-/**
- *
- * @author Jason
- */
 @Stateless
 public class ManufacturingWarehouseManagementBean implements ManufacturingWarehouseManagementBeanLocal {
 
+    @Override
+    public void createStorageBin(String type) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean updateStorageBin(StorageBinEntity storageBin) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteStorageBin(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public StorageBinEntity viewStorageBin(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<StorageBinEntity> viewAllStorageBin() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+    @Override
+    public boolean updateTransferOrder(TransferOrderEntity transferOrder) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public TransferOrderEntity viewTransferOrder(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteTransferOrder(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
+
+    @Override
+    public LineItemEntity createLineItem(ItemEntity item, Integer quantity, String packType) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void createTransferOrder(List<LineItemEntity> lineItems, StorageBinEntity origin, StorageBinEntity target) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
