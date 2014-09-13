eb9eee19c0594c2037ff14658e7fa7711feb231d
Yuan Guang <leeyuanguang.lyg@gmail.com>
2014-09-13 13:22:28 +0800

    LY TODO

--- IS3102_Project-ejb/src/java/CommonInfrastructure/AccountManagement/AccountManagementBeanLocal.java
+++ IS3102_Project-ejb/src/java/CommonInfrastructure/AccountManagement/AccountManagementBeanLocal.java
@@ -13,10 +13,13 @@ public interface AccountManagementBeanLocal {
 
     public boolean checkMemberEmailExists(String email);
     public boolean registerMember(String name, String address, Date DOB, String email, Integer phone, String country, String city, Integer zipCode, String password);
+    //public boolean editMember(Long memberID, String DOB, String name, String address, String email, Integer phone, String country, String city, Integer zipCode, String password);
     public MemberEntity loginMember(String email, String password);
 
     public boolean checkStaffEmailExists(String email);
     public StaffEntity registerStaff(String identificationNo, String name, Integer phone, String email, String address, String password);
+    //public boolean editStaff(Long staffID, String identificationNo, String name, String email, Integer phone, String password, String address);
+    //public boolean editStaff(Long staffID, String email, Integer phone, String password, String address);
     public StaffEntity loginStaff(String email, String password);
     public List<StaffEntity> listAllStaff();
     
