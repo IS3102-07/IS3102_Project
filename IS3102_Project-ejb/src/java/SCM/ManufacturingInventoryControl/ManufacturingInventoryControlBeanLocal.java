f4f6a8ebbf44ca6a864b24efa2fff9547ab05a34
Yuan Guang <leeyuanguang.lyg@gmail.com>
2014-09-13 23:19:54 +0800

    added func for manu inventory control

--- IS3102_Project-ejb/src/java/SCM/ManufacturingInventoryControl/ManufacturingInventoryControlBeanLocal.java
+++ IS3102_Project-ejb/src/java/SCM/ManufacturingInventoryControl/ManufacturingInventoryControlBeanLocal.java
@@ -2,13 +2,15 @@ package SCM.ManufacturingInventoryControl;
 
 import EntityManager.ItemEntity;
 import EntityManager.StorageBinEntity;
+import EntityManager.WarehouseEntity;
 import java.util.List;
 import javax.ejb.Local;
 
 @Local
 public interface ManufacturingInventoryControlBeanLocal {
 
-    public List<StorageBinEntity> getEmptyStorageBins(ItemEntity itemEntity); //returns the appropirate type of empty storage bins
+    //Finds the list of suitable storage bin that can hold the item based on the kind of item passed in
+    public List<StorageBinEntity> getEmptyStorageBins(Long warehouseID, ItemEntity itemEntity); //returns the appropirate type of empty storage bins
 //    public Integer calculateQtyOfItemsStorageBinHolds(StorageBinEntity storageBin, Integer volumeOfItem);
 //    
 //    
@@ -18,7 +20,7 @@ public interface ManufacturingInventoryControlBeanLocal {
 //    
     public boolean addItemToReceivingBin(String SKU);
     public boolean moveItemBetweenStorageBins(String SKU, StorageBinEntity source, StorageBinEntity destination);
-    public boolean addItemsToShippingBin(String SKU);
-    public int checkItemQty(String SKU);
-    public StorageBinEntity findStorageBinThatContainsItem(String SKU);
+    
+    public Integer checkItemQty(Long warehouseId, String SKU);
+    public List<StorageBinEntity> findStorageBinsThatContainsItem(Long warehouseId, String SKU);
 }
