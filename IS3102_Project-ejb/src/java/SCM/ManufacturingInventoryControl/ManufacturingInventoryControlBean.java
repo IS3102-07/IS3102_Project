659c223806c8b912c3530681e24e569ca994317a
Yuan Guang <leeyuanguang.lyg@gmail.com>
2014-09-13 20:37:50 +0800

    added functions for manufacturing inventory control

--- IS3102_Project-ejb/src/java/SCM/ManufacturingInventoryControl/ManufacturingInventoryControlBean.java
+++ IS3102_Project-ejb/src/java/SCM/ManufacturingInventoryControl/ManufacturingInventoryControlBean.java
@@ -6,6 +6,10 @@
 
 package SCM.ManufacturingInventoryControl;
 
+import EntityManager.ItemEntity;
+import EntityManager.MemberEntity;
+import EntityManager.StorageBinEntity;
+import java.util.List;
 import javax.ejb.Stateless;
 
 /**
@@ -14,7 +18,21 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class ManufacturingInventoryControlBean implements ManufacturingInventoryControlBeanLocal {
-
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+    public List<StorageBinEntity> getEmptyStorageBins(ItemEntity itemEntity){
+        return null;
+    }
+    
+    public boolean addItemToReceivingBin(String SKU){
+        System.out.println("addItemToStorageBin() called with SKU:" + SKU);
+        return true;
+    }
+    public boolean moveItemBetweenStorageBins(String SKU, StorageBinEntity source, StorageBinEntity destination){
+        return true;
+    }
+    public boolean addItemsToShippingBin(String SKU) {
+        return true;
+    }
+    public int checkItemQty(String SKU){
+        return 0;
+    }
 }
--- IS3102_Project-ejb/src/java/SCM/ManufacturingInventoryControl/ManufacturingInventoryControlBeanLocal.java
+++ IS3102_Project-ejb/src/java/SCM/ManufacturingInventoryControl/ManufacturingInventoryControlBeanLocal.java
@@ -1,18 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package SCM.ManufacturingInventoryControl;
 
+import EntityManager.ItemEntity;
+import EntityManager.StorageBinEntity;
+import java.util.List;
 import javax.ejb.Local;
 
-/**
- *
- * @author Jason
- */
 @Local
 public interface ManufacturingInventoryControlBeanLocal {
-    
+
+    public List<StorageBinEntity> getEmptyStorageBins(ItemEntity itemEntity); //returns the appropirate type of empty storage bins
+//    public Integer calculateQtyOfItemsStorageBinHolds(StorageBinEntity storageBin, Integer volumeOfItem);
+//    
+//    
+//    public boolean compareStorageBinWithItem(StorageBinEntity storageBin, ItemEntity itemEntity);
+//    
+//    
+//    
+    public boolean addItemToReceivingBin(String SKU);
+    public boolean moveItemBetweenStorageBins(String SKU, StorageBinEntity source, StorageBinEntity destination);
+    public boolean addItemsToShippingBin(String SKU);
+    public int checkItemQty(String SKU);
 }
