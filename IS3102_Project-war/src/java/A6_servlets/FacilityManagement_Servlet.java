c71d4b90e22b1f7bce12bf1e9be227a6cf1d2b30
Bo-Yee-Woods <boyeewoods@gmail.com>
2014-09-14 21:05:36 +0800

    facility management jsp  web.xml

--- IS3102_Project-war/src/java/A6_servlets/FacilityManagement_Servlet.java
+++ IS3102_Project-war/src/java/A6_servlets/FacilityManagement_Servlet.java
@@ -31,18 +31,10 @@ public class FacilityManagement_Servlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet FacilityManagement_Servlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet FacilityManagement_Servlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
+        
+        
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
