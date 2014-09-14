0659eab435edd2f36914326b4fe212b525deff6d
Bo-Yee-Woods <boyeewoods@gmail.com>
2014-09-14 15:06:03 +0800

    edit "purchaseOrder", "warehouse" relationship

--- IS3102_Project-ejb/src/java/EntityManager/PurchaseOrderEntity.java
+++ IS3102_Project-ejb/src/java/EntityManager/PurchaseOrderEntity.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
@@ -34,7 +35,7 @@ public class PurchaseOrderEntity implements Serializable {
     @Temporal(value = TemporalType.DATE)
     private Date expectedReceivedDate;
     private Time deliveryTime;
-    @OneToOne
+    @ManyToOne
     private WarehouseEntity destination;
     private String contactName;
     private Integer contactNumber;
