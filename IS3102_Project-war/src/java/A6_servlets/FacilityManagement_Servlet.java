6d1d0c7703c9601ed0e8ac8bd68c57f3e70d0e0e
Bo-Yee-Woods <boyeewoods@gmail.com>
2014-09-15 22:46:39 +0800

    edit "servlet"

--- IS3102_Project-war/src/java/A6_servlets/FacilityManagement_Servlet.java
+++ IS3102_Project-war/src/java/A6_servlets/FacilityManagement_Servlet.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package A6_servlets;
 
+import CorporateManagement.FacilityManagement.FacilityManagementBeanLocal;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
+import javax.ejb.EJB;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -22,34 +22,52 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class FacilityManagement_Servlet extends HttpServlet {
 
-    
+    @EJB
+    private FacilityManagementBeanLocal fmBean;
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        
-        String nextPage = "";
+
+        String nextPage = "/A6/facilityManagement";
         ServletContext servletContext = getServletContext();
         RequestDispatcher dispatcher;
         SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
 
-        String target = request.getPathInfo();                
-        
+        String target = request.getPathInfo();
+
         switch (target) {
-            
+
             case "/warehouseManagement_index":
-                
-                
+
                 nextPage = "/A6/warehouseManagement";
                 break;
-            
-            case "/createWarehouse_GET":                
-                nextPage = "";
+
+            case "/createWarehouse_GET":
+                nextPage = "/A6/createWarehouse";
                 break;
-            
-            
+
+            case "/createWarehouse_POST":
+                try {
+                    String warehouseName = request.getParameter("warehouseName");
+                    String address = request.getParameter("address");
+                    String telephone = request.getParameter("telephone");
+                    String email = request.getParameter("email");
+
+                    if (fmBean.createWarehouse(warehouseName, address, telephone, email) != null) {
+                        request.setAttribute("alertMessage", "A new warehouse record has been saved.");
+                    } else {
+                        request.setAttribute("alertMessage", "Fail to create warehouse due to duplicated warehouse name.");
+                    }
+                    nextPage = "/A6/warehouseManagement";
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                break;
+
         }
-        dispatcher = servletContext.getRequestDispatcher(nextPage);        
-        dispatcher.forward(request, response);                
+        dispatcher = servletContext.getRequestDispatcher(nextPage);
+        dispatcher.forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
