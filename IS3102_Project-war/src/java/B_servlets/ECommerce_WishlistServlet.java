db1ef0936f140f790e43303e75ca09b5e8426bb8
Bo-Yee-Woods <boyeewoods@gmail.com>
2014-09-15 20:35:06 +0800

    edit web.xml

--- IS3102_Project-war/src/java/B_servlets/ECommerce_WishlistServlet.java
+++ IS3102_Project-war/src/java/B_servlets/ECommerce_WishlistServlet.java
@@ -1,83 +1,83 @@
-package B_servlets;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.ejb.EJB;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-//this class is like shopping cart
-public class ECommerce_WishlistServlet extends HttpServlet {
-    
-    @EJB
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        
-        
-        try {
-            HttpSession session = request.getSession();
-            
-            // Retrieve member session
-            // Retrieve wishlist session
-            
-            // if wishlist is empty
-                 // add item to wishlist
-            // else check if products is already in wishlist 
-            
-            
-            
-            // after adding save it to session
-            // session.setAttribute("wishlist", whatobject);
-            // response.sendRedirect("whatURL");
-            
-        } catch (Exception ex) {
-            out.println("\n\nError Message " + ex.getMessage());
-        } finally {
-            out.close();
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
-}
+//package B_servlets;
+//
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//import javax.ejb.EJB;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
+//
+////this class is like shopping cart
+//public class ECommerce_WishlistServlet extends HttpServlet {
+//    
+//    @EJB
+//    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        response.setContentType("text/html;charset=UTF-8");
+//        PrintWriter out = response.getWriter();
+//        
+//        
+//        try {
+//            HttpSession session = request.getSession();
+//            
+//            // Retrieve member session
+//            // Retrieve wishlist session
+//            
+//            // if wishlist is empty
+//                 // add item to wishlist
+//            // else check if products is already in wishlist 
+//            
+//            
+//            
+//            // after adding save it to session
+//            // session.setAttribute("wishlist", whatobject);
+//            // response.sendRedirect("whatURL");
+//            
+//        } catch (Exception ex) {
+//            out.println("\n\nError Message " + ex.getMessage());
+//        } finally {
+//            out.close();
+//        }
+//    }
+//
+//    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+//    /**
+//     * Handles the HTTP <code>GET</code> method.
+//     *
+//     * @param request servlet request
+//     * @param response servlet response
+//     * @throws ServletException if a servlet-specific error occurs
+//     * @throws IOException if an I/O error occurs
+//     */
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        processRequest(request, response);
+//    }
+//
+//    /**
+//     * Handles the HTTP <code>POST</code> method.
+//     *
+//     * @param request servlet request
+//     * @param response servlet response
+//     * @throws ServletException if a servlet-specific error occurs
+//     * @throws IOException if an I/O error occurs
+//     */
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        processRequest(request, response);
+//    }
+//
+//    /**
+//     * Returns a short description of the servlet.
+//     *
+//     * @return a String containing servlet description
+//     */
+//    @Override
+//    public String getServletInfo() {
+//        return "Short description";
+//    }// </editor-fold>
+//
+//}
