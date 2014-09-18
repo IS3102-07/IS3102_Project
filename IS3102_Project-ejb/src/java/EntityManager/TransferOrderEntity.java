7c9627c6070b0fbc3a3d414d34ee3f9c2fce1081
zhengxiaodong1991 <a0099518@nus.edu.sg>
2014-09-18 11:50:07 +0800

    TransferOrder ManyToOne warehouse

--- IS3102_Project-ejb/src/java/EntityManager/TransferOrderEntity.java
+++ IS3102_Project-ejb/src/java/EntityManager/TransferOrderEntity.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 
@@ -28,7 +29,7 @@ public class TransferOrderEntity implements Serializable {
     @OneToOne
     private StorageBinEntity target;
     private String status;
-    @OneToOne
+    @ManyToOne
     private WarehouseEntity warehouse;
     
 
