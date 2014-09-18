1f7621d35f53f622ff77a3058ec4e7a6fde64a00
Neo Wei <neowei03@gmail.com>
2014-09-18 12:26:07 +0800

    done with storage management

--- IS3102_Project-war/src/java/A3_servlets/TransferOrderManagement_Servlet.java
+++ IS3102_Project-war/src/java/A3_servlets/TransferOrderManagement_Servlet.java
@@ -1,50 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package A3_servlets;
 
+import EntityManager.TransferOrderEntity;
+import EntityManager.WarehouseEntity;
+import SCM.ManufacturingWarehouseManagement.ManufacturingWarehouseManagementBeanLocal;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author Neo
- */
 public class TransferOrderManagement_Servlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    @EJB
+    private ManufacturingWarehouseManagementBeanLocal manufacturingWarehouseManagementBean;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet TransferOrderManagement_Servlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet TransferOrderManagement_Servlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        PrintWriter out = response.getWriter();
+
+        try {
+            HttpSession session;
+            session = request.getSession();
+            String errMsg = request.getParameter("errMsg");
+            WarehouseEntity warehouseEntity = (WarehouseEntity) (session.getAttribute("warehouseEntity"));
+
+            //List<TransferOrderEntity> transferOrders = manufacturingWarehouseManagementBean.viewListOfAllTransferOrder()warehouseEntity.getId());
+//            session.setAttribute("transferOrders", transferOrders);
+//            if (errMsg == null || errMsg.equals("")) {
+//                response.sendRedirect("A3/transferOrderManagement.jsp");
+//            } else {
+//                response.sendRedirect("A3/transferOrderManagement.jsp?errMsg=" + errMsg);
+//            }
+        } catch (Exception ex) {
+            out.println("\n\n " + ex.getMessage());
         }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
     /**
      * Handles the HTTP <code>GET</code> method.
      *
