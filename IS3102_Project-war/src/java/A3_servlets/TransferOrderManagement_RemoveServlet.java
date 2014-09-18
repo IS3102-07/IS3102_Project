e61a97d93bcf66a1e2934f53b3f8520e133ce6d2
Neo Wei <neowei03@gmail.com>
2014-09-18 17:23:26 +0800

    done with create TO and delete TO

--- IS3102_Project-war/src/java/A3_servlets/TransferOrderManagement_RemoveServlet.java
+++ IS3102_Project-war/src/java/A3_servlets/TransferOrderManagement_RemoveServlet.java
@@ -1,46 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package A3_servlets;
 
+import SCM.ManufacturingWarehouseManagement.ManufacturingWarehouseManagementBeanLocal;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Neo
- */
 public class TransferOrderManagement_RemoveServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    @EJB
+    private ManufacturingWarehouseManagementBeanLocal manufacturingWarehouseManagementBean;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet TransferOrderManagement_RemoveServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet TransferOrderManagement_RemoveServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        try {
+
+            String[] deleteArr = request.getParameterValues("delete");
+            if (deleteArr != null) {
+                for (int i = 0; i < deleteArr.length; i++) {
+                    manufacturingWarehouseManagementBean.deleteTransferOrder(Long.parseLong(deleteArr[i]));
+                }
+                response.sendRedirect("TransferOrderManagement_Servlet?errMsg=Successfully removed: " + deleteArr.length + " record(s).");
+            } else {
+                response.sendRedirect("A3/TransferOrderManagement_Servlet.jsp?errMsg=Nothing is selected.");
+            }
+
+        } catch (Exception ex) {
+            out.println(ex);
         }
     }
 
