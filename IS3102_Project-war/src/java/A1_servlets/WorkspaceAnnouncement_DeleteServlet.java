13e2b0d53fea1268bdfd148947106c3e231dbbbc
Loi Liang Yang <loiliangyang@gmail.com>
2014-09-19 15:56:28 +0800

    Work space announcement delete servlet completed.

--- IS3102_Project-war/src/java/A1_servlets/WorkspaceAnnouncement_DeleteServlet.java
+++ IS3102_Project-war/src/java/A1_servlets/WorkspaceAnnouncement_DeleteServlet.java
@@ -5,8 +5,12 @@
  */
 package A1_servlets;
 
+import CommonInfrastructure.Workspace.WorkspaceBeanLocal;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.Date;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,30 +22,27 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class WorkspaceAnnouncement_DeleteServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    @EJB
+    private WorkspaceBeanLocal workspaceBeanLocal;
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet WorkspaceAnnouncement_DeleteServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet WorkspaceAnnouncement_DeleteServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        String result;
+        PrintWriter out = response.getWriter();
+        try {
+            String id = request.getParameter("id");
+            if (workspaceBeanLocal.deleteAnnouncement(Long.valueOf(id))) {
+                result = "?errMsg=Announcement deleted.";
+                response.sendRedirect("A1/workspace_BroadcastAnnouncement.jsp" + result);
+            } else {
+                result = "?errMsg=Failed to delete announcement.";
+                response.sendRedirect("A1/workspace_BroadcastAnnouncement.jsp" + result);
+            }
+        } catch (Exception ex) {
+            out.println(ex);
         }
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
