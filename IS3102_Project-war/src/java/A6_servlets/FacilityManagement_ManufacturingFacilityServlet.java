4d0ed4883727d6749c9dbad175ab3c8b5b665170
Loi Liang Yang <loiliangyang@gmail.com>
2014-09-21 23:23:56 +0800

    store management done

--- IS3102_Project-war/src/java/A6_servlets/FacilityManagement_ManufacturingFacilityServlet.java
+++ IS3102_Project-war/src/java/A6_servlets/FacilityManagement_ManufacturingFacilityServlet.java
@@ -3,48 +3,115 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package A6_servlets;
 
+import CorporateManagement.FacilityManagement.FacilityManagementBeanLocal;
+import EntityManager.ManufacturingFacilityEntity;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author yang
+ * @author Administrator
  */
-@WebServlet(name = "FacilityManagement_ManufacturingFacilityServlet", urlPatterns = {"/FacilityManagement_ManufacturingFacilityServlet/*"})
 public class FacilityManagement_ManufacturingFacilityServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    @EJB
+    private FacilityManagementBeanLocal fmBean;
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet FacilityManagement_ManufacturingFacilityServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet FacilityManagement_ManufacturingFacilityServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+
+        String nextPage = "/A6/facilityManagement";
+        ServletContext servletContext = getServletContext();
+        RequestDispatcher dispatcher;
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+
+        String target = request.getPathInfo();
+
+        switch (target) {
+
+            case "/manufacturingFacilityManagement_index":
+                List<ManufacturingFacilityEntity> manufacturingFacilityEntity = fmBean.viewListOfManufacturingFacility();
+                request.setAttribute("manufacturingFacilityEntity", manufacturingFacilityEntity);
+                nextPage = "/A6/manufacturingFacilityManagement";
+                break;
+
+            case "/createManufacturingFacility_GET":
+                System.out.println("Create manufacturing facility in servlet");
+                String submit_btn = request.getParameter("submit-btn");
+                if (submit_btn.equals("Add Manufacturing Facility")) {
+                    nextPage = "/A6/createManufacturingFacility";
+                } else if (submit_btn.equals("Delete Manufacturing Facility")) {
+                    nextPage = "/FacilityManagement_ManufacturingFacilityServlet/deleteManufacturingFacility";
+                }
+                break;
+
+            case "/createManufacturingFacility_POST":
+                try {
+                    String manufacturingFacilityName = request.getParameter("manufacturingFacilityName");
+                    String address = request.getParameter("address");
+                    String telephone = request.getParameter("telephone");
+                    String email = request.getParameter("email");
+
+                    ManufacturingFacilityEntity manufacturingFacility = fmBean.createManufacturingFacility(manufacturingFacilityName);
+                    if (manufacturingFacility != null) {
+                        request.setAttribute("alertMessage", "A new manufacturing facility record has been saved.");
+                    } else {
+                        request.setAttribute("alertMessage", "Fail to create manufacturing facility due to duplicated name.");
+                    }
+                    request.setAttribute("manufacturingFacility", manufacturingFacility);
+                    nextPage = "/FacilityManagement_ManufacturingFacilityServlet/manufacturingFacilityManagement_index";
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                break;
+
+            case "/editManufacturingFacility_GET":
+                String manufacturingFacilityId = request.getParameter("delete");
+                System.out.println("Manufacturing Facility ID is " + manufacturingFacilityId);
+                ManufacturingFacilityEntity manufacturingFacility = fmBean.viewManufacturingFacility(manufacturingFacilityId);
+                request.setAttribute("manufacturingFacility", manufacturingFacility);
+                nextPage = "/A6/editManufacturingFacility";
+                break;
+
+            case "/editManufacturingFacility_POST":
+                String manufacturingFacilityName = request.getParameter("manufacturingFacilityName");
+                String address = request.getParameter("address");
+                String telephone = request.getParameter("telephone");
+                String email = request.getParameter("email");
+                Long id = Long.parseLong(request.getParameter("regionalOfficeId"));
+
+                if (fmBean.editManufacturingFacility(id, manufacturingFacilityName)) {
+                    request.setAttribute("alertMessage", "The manufacturing facility has been saved.");
+                } else {
+                    request.setAttribute("alertMessage", "Fail to edit manufacturing facility.");
+                }
+                nextPage = "/FacilityManagement_ManufacturingFacilityServlet/manufacturingFacilityManagement_index";
+                break;
+            case "/deleteManufacturingFacility":
+                String[] deletes = request.getParameterValues("delete");
+                if (deletes != null) {
+                    for (String manufacturingFacilityString : deletes) {
+                        String manufacturingFacility_Id = manufacturingFacilityString;
+                        fmBean.removeManufacturingFacility(manufacturingFacility_Id);
+                    }
+                }
+                nextPage = "/FacilityManagement_ManufacturingFacilityServlet/manufacturingFacilityManagement_index";
+                break;
+
         }
+        dispatcher = servletContext.getRequestDispatcher(nextPage);
+        dispatcher.forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
