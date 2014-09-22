1eab69ba96a4ef36ad734408601bb52b1cd84e18
Jason Sim <simmengkiat@gmail.com>
2014-09-22 17:09:28 +0800

    Updated the methods in ItemManagemenBean for BOM

--- IS3102_Project-ejb/src/java/EntityManager/BillOfMaterialEntity.java
+++ IS3102_Project-ejb/src/java/EntityManager/BillOfMaterialEntity.java
@@ -1,7 +1,9 @@
 package EntityManager;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -20,10 +22,11 @@ public class BillOfMaterialEntity implements Serializable {
     private String description;
     @OneToOne
     FurnitureEntity furniture;
-    @OneToMany
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.REMOVE, CascadeType.MERGE, CascadeType.REFRESH})
     List<LineItemEntity> listOfLineItems;
 
     public BillOfMaterialEntity() {
+        listOfLineItems = new ArrayList<>();
     }
 
     public Long getId() {
