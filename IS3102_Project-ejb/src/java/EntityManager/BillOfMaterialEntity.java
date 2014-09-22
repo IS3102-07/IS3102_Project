ee070a0a9909396e2c25f0837f4a2f4dd6047a23
Jason Sim <simmengkiat@gmail.com>
2014-09-22 16:20:27 +0800

    Added name and description for BOMEntity

--- IS3102_Project-ejb/src/java/EntityManager/BillOfMaterialEntity.java
+++ IS3102_Project-ejb/src/java/EntityManager/BillOfMaterialEntity.java
@@ -16,6 +16,7 @@ public class BillOfMaterialEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String name;
     private String description;
     @OneToOne
     FurnitureEntity furniture;
@@ -31,6 +32,14 @@ public class BillOfMaterialEntity implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getDescription() {
