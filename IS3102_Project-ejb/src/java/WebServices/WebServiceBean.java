783a9dc128ae1a4745c9c430fa33c6ed297905e0
Lee Yuan Guang <leeyuanguang.lyg@gmail.com>
2014-10-06 22:03:29 +0800

    getStoreByID() web service

--- IS3102_Project-ejb/src/java/WebServices/WebServiceBean.java
+++ IS3102_Project-ejb/src/java/WebServices/WebServiceBean.java
@@ -1,7 +1,9 @@
 package WebServices;
 
 import CommonInfrastructure.AccountManagement.AccountManagementBeanLocal;
+import CorporateManagement.FacilityManagement.FacilityManagementBeanLocal;
 import EntityManager.StaffEntity;
+import EntityManager.StoreEntity;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.jws.WebMethod;
@@ -14,6 +16,9 @@ public class WebServiceBean {
 
     @EJB
     AccountManagementBeanLocal AccountManagementBeanLocal;
+    
+    @EJB
+    FacilityManagementBeanLocal FacilityManagementBeanLocal;
 
     @WebMethod
     public Long loginStaff(@WebParam(name = "email") String email, @WebParam(name = "password") String password) {
@@ -21,5 +26,10 @@ public class WebServiceBean {
         Long staffID = staffEntity.getId();
         return staffID;
     }
-
+    
+    @WebMethod
+    public StoreEntity getStoreByID(@WebParam(name = "storeID") Long storeID) {
+        StoreEntity storeEntity = FacilityManagementBeanLocal.getStoreByID(storeID);
+        return storeEntity;
+    }
 }
