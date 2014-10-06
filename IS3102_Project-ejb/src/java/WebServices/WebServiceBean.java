63edabb23edbcbf5071e803d0a6c1f5ae57919e7
Lee Yuan Guang <leeyuanguang.lyg@gmail.com>
2014-10-06 22:37:46 +0800

    POSloginStaff only allows Administrator, Store manager or Cashier

--- IS3102_Project-ejb/src/java/WebServices/WebServiceBean.java
+++ IS3102_Project-ejb/src/java/WebServices/WebServiceBean.java
@@ -2,8 +2,10 @@ package WebServices;
 
 import CommonInfrastructure.AccountManagement.AccountManagementBeanLocal;
 import CorporateManagement.FacilityManagement.FacilityManagementBeanLocal;
+import EntityManager.RoleEntity;
 import EntityManager.StaffEntity;
 import EntityManager.StoreEntity;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.jws.WebMethod;
@@ -16,17 +18,24 @@ public class WebServiceBean {
 
     @EJB
     AccountManagementBeanLocal AccountManagementBeanLocal;
-    
+
     @EJB
     FacilityManagementBeanLocal FacilityManagementBeanLocal;
 
     @WebMethod
-    public Long loginStaff(@WebParam(name = "email") String email, @WebParam(name = "password") String password) {
+    public Long POSloginStaff(@WebParam(name = "email") String email, @WebParam(name = "password") String password) {
         StaffEntity staffEntity = AccountManagementBeanLocal.loginStaff(email, password);
-        Long staffID = staffEntity.getId();
-        return staffID;
+        // Check roles, only cashier or store manager can login into POS
+        List<RoleEntity> roles = staffEntity.getRoles();
+        for (RoleEntity role : roles) {
+            if (role.getId().equals(1L) || role.getId().equals(4L) || role.getId().equals(9L)) {
+                Long staffID = staffEntity.getId();
+                return staffID;
+            }
+        }
+        return null;
     }
-    
+
     @WebMethod
     public StoreEntity getStoreByID(@WebParam(name = "storeID") Long storeID) {
         StoreEntity storeEntity = FacilityManagementBeanLocal.getStoreByID(storeID);
