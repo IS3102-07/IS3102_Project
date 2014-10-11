80d71d86e9cf6ed8da5af1210d5b987d800284fb
Lee Yuan Guang <leeyuanguang.lyg@gmail.com>
2014-10-11 14:36:48 +0800

    SMS

--- IS3102_Project-ejb/src/java/WebServices/StoreWebServiceBean.java
+++ IS3102_Project-ejb/src/java/WebServices/StoreWebServiceBean.java
@@ -10,6 +10,7 @@ import EntityManager.StoreEntity;
 import HelperClasses.ReturnHelper;
 import OperationalCRM.LoyaltyAndRewards.LoyaltyAndRewardsBeanLocal;
 import StoreTransaction.RetailInventoryControl.RetailInventoryControlBeanLocal;
+import java.io.OutputStream;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -70,7 +71,7 @@ public class StoreWebServiceBean {
     }
 
     @WebMethod
-    public ReturnHelper submitSalesRecord(@WebParam(name = "staffEmail") String staffEmail, @WebParam(name = "password") String staffPasword, @WebParam(name = "storeID") Long storeID, @WebParam(name = "posName") String posName, @WebParam(name = "itemsPurchased") List<LineItemEntity> itemsPurchased, @WebParam(name="amountPaid") Double paymentAmount, @WebParam(name="memberEmail") String memberEmail) {
+    public ReturnHelper submitSalesRecord(@WebParam(name = "staffEmail") String staffEmail, @WebParam(name = "password") String staffPasword, @WebParam(name = "storeID") Long storeID, @WebParam(name = "posName") String posName, @WebParam(name = "itemsPurchased") List<LineItemEntity> itemsPurchased, @WebParam(name = "amountPaid") Double paymentAmount, @WebParam(name = "memberEmail") String memberEmail) {
         //TODO
         return new ReturnHelper(true, "Checkout successful.");
     }
@@ -84,7 +85,33 @@ public class StoreWebServiceBean {
     @WebMethod
     public Boolean alertSupervisor(@WebParam(name = "posName") String posName, @WebParam(name = "supervisorTel") Integer telNo) {
         try {
-            //send SMS code TODO
+//            //send SMS code TODO
+//            String smsMessage = "[Island Furniture] POS:\"" + posName + "\" requires assistance.";
+//            System.out.println("Sending SMS: " + telNo + ": " + smsMessage);
+//
+//            String initString1 = "AT" + (char) 13;
+//            String initString2 = "AT+CMGF=1" + (char) 13;
+//            String cmdString1 = "AT+CMGS=" + telNo.toString() + (char) 13;
+//            String cmdString2 = smsMessage + (char) 26;
+//
+//            System.out.println("initString1: " + initString1);
+//            System.out.println("initString2: " + initString2);
+//            System.out.println("cmdString1: " + cmdString1);
+//            System.out.println("cmdString2: " + cmdString2);
+//
+//            CommPortIdentifier commPortIdentifier = CommPortIdentifier.getPortIdentifier("COM9");
+//            SerialPort serialPort = (SerialPort) commPortIdentifier.open("SMS", 2000);
+//            serialPort.setSerialPortParams(9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+//            OutputStream outputStream = serialPort.getOutputStream();
+//
+//            outputStream.write(initString1.getBytes());
+//            outputStream.write(initString2.getBytes());
+//            outputStream.write(cmdString1.getBytes());
+//            outputStream.write(cmdString2.getBytes());
+//
+//            Thread.sleep(2000);
+//
+//            serialPort.close();
             return true;
         } catch (Exception ex) {
             return false;
