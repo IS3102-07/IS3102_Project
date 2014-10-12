2a057eecce660b1a7bd53c5b83341b3bc6f8872e
Bo-Yee-Woods <boyeewoods@gmail.com>
2014-10-12 12:13:39 +0800

    demand management

--- IS3102_Project-ejb/src/java/MRP/DemandManagement/DemandManagementBean.java
+++ IS3102_Project-ejb/src/java/MRP/DemandManagement/DemandManagementBean.java
@@ -25,39 +25,64 @@ public class DemandManagementBean implements DemandManagementBeanLocal {
 
     @Override
     public Boolean generateMasterProductionSchedules(Long MfId) {
+        System.out.println("generateMasterProductionSchedules is called.");
         try {
             Query q = em.createQuery("select s from MonthScheduleEntity s");
             List<MonthScheduleEntity> scheduleList = q.getResultList();
             if (!scheduleList.isEmpty()) {
                 ManufacturingFacilityEntity mf = em.find(ManufacturingFacilityEntity.class, MfId);
                 MonthScheduleEntity lastSchedule = scheduleList.get(scheduleList.size() - 1);
+                
+                // clear former MPSs
+                Query q1 = em.createQuery("select mps from MasterProductionScheduleEntity mps where mps.mf.id = ?1 and mps.schedule.id = ?2")
+                                .setParameter(1, MfId)
+                                .setParameter(2, lastSchedule.getId());
+                List<MasterProductionScheduleEntity> formerMPSs = (List<MasterProductionScheduleEntity>) q1.getResultList();
+                for(MasterProductionScheduleEntity mps: formerMPSs){
+                    em.remove(mps);                    
+                }
+                em.flush();
+                
+                // generate MPSs
                 Query q2 = em.createQuery("select sop from SaleAndOperationPlanEntity sop where sop.schedule.id = ?1 and sop.manufacturingFacility.id = ?2")
                         .setParameter(1, lastSchedule.getId())
                         .setParameter(2, MfId);
-                List<SaleAndOperationPlanEntity> sopList = (List<SaleAndOperationPlanEntity>) q2.getResultList();
+                List<SaleAndOperationPlanEntity> sopList = (List<SaleAndOperationPlanEntity>) q2.getResultList();                
+                System.out.println("sopList.size(): " + sopList.size());
+                
                 for (SaleAndOperationPlanEntity sop : sopList) {
-                    for (ProductGroupLineItemEntity lineitem : sop.getProductGroup().getLineItemList()) {
+                    int residualMonthlyProductAmount = sop.getProductionPlan();
+                    List<ProductGroupLineItemEntity> lineItemList = sop.getProductGroup().getLineItemList();
+                    for (ProductGroupLineItemEntity lineitem : lineItemList) {
                         Query q3 = em.createQuery("select mps from MasterProductionScheduleEntity mps where mps.mf.id = ?1 and mps.schedule.id = ?2 and mps.furniture.SKU = ?3")
                                 .setParameter(1, MfId)
                                 .setParameter(2, lastSchedule.getId())
                                 .setParameter(3, lineitem.getFurniture().getSKU());
 
                         MasterProductionScheduleEntity mps;
-
+                        Boolean mpsExits;
                         if (q3.getResultList().isEmpty()) {
                             mps = new MasterProductionScheduleEntity();
                             mps.setMf(mf);
                             mps.setSchedule(lastSchedule);
                             mps.setFurniture(lineitem.getFurniture());
-
+                            mpsExits = true;                            
                         } else {
                             mps = (MasterProductionScheduleEntity) q3.getResultList().get(0);
+                            mpsExits = false;                            
                         }
 
                         int days_month = lastSchedule.getWorkDays_firstWeek() + lastSchedule.getWorkDays_secondWeek() + lastSchedule.getWorkDays_thirdWeek()
                                 + lastSchedule.getWorkDays_forthWeek() + lastSchedule.getWorkDays_fifthWeek();
 
-                        int amount = (int) Math.round(sop.getProductionPlan() * lineitem.getPercent());
+                        int amount = 0;
+                        if (!lineitem.getId().equals(lineItemList.get(lineItemList.size() - 1).getId())) {
+                            amount = (int) Math.round(sop.getProductionPlan() * lineitem.getPercent());
+                            residualMonthlyProductAmount -= amount;
+                        }else{
+                            amount = residualMonthlyProductAmount;
+                        }
+                                                
                         int amount_week1 = (int) Math.round(1.0 * amount * lastSchedule.getWorkDays_firstWeek() / days_month);
                         int amount_week2 = (int) Math.round(1.0 * amount * lastSchedule.getWorkDays_firstWeek() / days_month);
                         int amount_week3 = (int) Math.round(1.0 * amount * lastSchedule.getWorkDays_firstWeek() / days_month);
@@ -73,7 +98,11 @@ public class DemandManagementBean implements DemandManagementBeanLocal {
                             mps.setAmount_week4(mps.getAmount_week4() + amount_week4);
                             mps.setAmount_week5(mps.getAmount_week5() + amount - amount_week1 - amount_week2 - amount_week3 - amount_week4);
                         }
-                        em.merge(mps);
+                        if (mpsExits) {
+                            em.merge(mps);
+                        } else {
+                            em.persist(mps);
+                        }
                     }
                 }
             }
@@ -83,7 +112,7 @@ public class DemandManagementBean implements DemandManagementBeanLocal {
         }
         return false;
     }
-    
+
     @Override
     public List<MasterProductionScheduleEntity> getMasterProductionSchedules(Long MfId) {
         try {
@@ -103,5 +132,4 @@ public class DemandManagementBean implements DemandManagementBeanLocal {
         return new ArrayList<>();
     }
 
-    
 }
