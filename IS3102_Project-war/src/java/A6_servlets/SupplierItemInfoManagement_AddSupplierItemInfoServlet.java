e9b27773e580ad955adfcc0d7422a87c56a6b70f
Yuan Guang <leeyuanguang.lyg@gmail.com>
2014-10-12 22:20:19 +0800

    Completed SupplierItemInfoManagement

--- IS3102_Project-war/src/java/A6_servlets/SupplierItemInfoManagement_AddSupplierItemInfoServlet.java
+++ IS3102_Project-war/src/java/A6_servlets/SupplierItemInfoManagement_AddSupplierItemInfoServlet.java
@@ -1,46 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package A6_servlets;
 
+import CorporateManagement.ItemManagement.ItemManagementBeanLocal;
+import HelperClasses.ReturnHelper;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author -VeRyLuNaTiC
- */
 public class SupplierItemInfoManagement_AddSupplierItemInfoServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    @EJB
+    private ItemManagementBeanLocal itemManagementBeanLocal;
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet SupplierItemInfoManagement_AddSupplierItemInfoServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet SupplierItemInfoManagement_AddSupplierItemInfoServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        try{
+            String sku = request.getParameter("sku");
+            String supplierId = request.getParameter("supplierId");
+            String costPrice = request.getParameter("costPrice");
+            String lotSize = request.getParameter("lotSize");
+            String leadTime = request.getParameter("leadTime");
+            ReturnHelper helper = itemManagementBeanLocal.addSupplierItemInfo(sku, Long.parseLong(supplierId), Double.parseDouble(costPrice), Integer.parseInt(lotSize), Integer.parseInt(leadTime));
+           
+            if (!helper.getIsSuccess()) {
+                response.sendRedirect("SupplierItemInfoManagement_Servlet?errMsg=" + helper.getMessage());
+            } else {
+                response.sendRedirect("SupplierItemInfoManagement_Servlet?errMsg=" + helper.getMessage());
+            }
+        }catch(Exception ex){
+            ex.printStackTrace();
+            response.sendRedirect("SupplierItemInfoManagement_Servlet?errMsg=An error has occurred. Please try again.");
         }
     }
 
