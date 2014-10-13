6884cb6b713cbc8d2b38d7f31b7ec589c79ed51a
Loi Liang Yang <loiliangyang@gmail.com>
2014-10-13 20:29:25 +0800

    added to xml

--- IS3102_Project-war/src/java/B_servlets/ECommerce_SendResetPasswordServlet.java
+++ IS3102_Project-war/src/java/B_servlets/ECommerce_SendResetPasswordServlet.java
@@ -1,47 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package B_servlets;
 
+import CommonInfrastructure.AccountManagement.AccountManagementBeanLocal;
+import CommonInfrastructure.SystemSecurity.SystemSecurityBeanLocal;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author yang
- */
 public class ECommerce_SendResetPasswordServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    @EJB
+    private SystemSecurityBeanLocal systemSecurityBean;
+    private String result;
+    @EJB
+    private AccountManagementBeanLocal accountManagementBean;
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ECommerce_SendResetPasswordServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ECommerce_SendResetPasswordServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        try {
+            String email = request.getParameter("email");
+
+            boolean ifExist = accountManagementBean.checkStaffEmailExists(email);
+            if (ifExist) {
+                systemSecurityBean.sendPasswordResetEmailForMember(email);
+                result = "?goodMsg=Send email successful. Please enter your activation code to reset your password.&email=" + email;
+                response.sendRedirect("./B/memberResetPassword.jsp" + result);
+            } else {
+                result = "?errMsg=Staff email does not exist.";
+                response.sendRedirect("./B/memberResetPassword.jsp" + result);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        } finally {
+            System.out.close();
         }
     }
 
--- IS3102_Project-war/web/B/forgotPassword.jsp
+++ IS3102_Project-war/web/B/forgotPassword.jsp
@@ -63,7 +63,7 @@
                                 <div class="featured-box featured-box-secundary default info-content">
                                     <div class="box-content">
                                         <h4>Forgot Password</h4>
-                                        <form action="../ECommerce_ResetPasswordServlet">
+                                        <form action="../ECommerce_SendResetPasswordServlet">
                                             <div class="row">
                                                 <div class="form-group">
                                                     <div class="col-md-12">
