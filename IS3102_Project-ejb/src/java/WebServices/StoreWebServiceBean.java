12e30d75541fd1383984a54aec90fef574694a43
Yuan Guang <leeyuanguang.lyg@gmail.com>
2014-10-14 02:52:59 +0800

    SMS web svc

--- IS3102_Project-ejb/src/java/WebServices/StoreWebServiceBean.java
+++ IS3102_Project-ejb/src/java/WebServices/StoreWebServiceBean.java
@@ -11,6 +11,12 @@ import HelperClasses.ItemHelper;
 import HelperClasses.ReturnHelper;
 import OperationalCRM.LoyaltyAndRewards.LoyaltyAndRewardsBeanLocal;
 import StoreTransaction.RetailInventoryControl.RetailInventoryControlBeanLocal;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -75,41 +81,33 @@ public class StoreWebServiceBean {
     }
 
     @WebMethod
-    public Boolean callSupervisor(@WebParam(name = "contactNo") String contactNo) {
-        //TODO
-        return true;
-    }
-
-    @WebMethod
     public Boolean alertSupervisor(@WebParam(name = "posName") String posName, @WebParam(name = "supervisorTel") Integer telNo) {
         try {
-//            //send SMS code TODO
-//            String smsMessage = "[Island Furniture] POS:\"" + posName + "\" requires assistance.";
-//            System.out.println("Sending SMS: " + telNo + ": " + smsMessage);
-//
-//            String initString1 = "AT" + (char) 13;
-//            String initString2 = "AT+CMGF=1" + (char) 13;
-//            String cmdString1 = "AT+CMGS=" + telNo.toString() + (char) 13;
-//            String cmdString2 = smsMessage + (char) 26;
-//
-//            System.out.println("initString1: " + initString1);
-//            System.out.println("initString2: " + initString2);
-//            System.out.println("cmdString1: " + cmdString1);
-//            System.out.println("cmdString2: " + cmdString2);
-//
-//            CommPortIdentifier commPortIdentifier = CommPortIdentifier.getPortIdentifier("COM9");
-//            SerialPort serialPort = (SerialPort) commPortIdentifier.open("SMS", 2000);
-//            serialPort.setSerialPortParams(9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-//            OutputStream outputStream = serialPort.getOutputStream();
-//
-//            outputStream.write(initString1.getBytes());
-//            outputStream.write(initString2.getBytes());
-//            outputStream.write(cmdString1.getBytes());
-//            outputStream.write(cmdString2.getBytes());
-//
-//            Thread.sleep(2000);
-//
-//            serialPort.close();
+            StringBuffer response;
+            String smsMessage = "[Island Furniture] POS:\"" + posName + "\" requires assistance.";
+            System.out.println("Sending SMS: " + telNo + ": " + smsMessage);
+
+            String RequestURL = "http://www.redoxygen.net/sms.dll?Action=SendSMS";
+
+            String Data = ("AccountId=" + URLEncoder.encode("CI00136959", "UTF-8"));
+            Data += ("&Email=" + URLEncoder.encode("lyg@nus.edu.sg", "UTF-8"));
+            Data += ("&Password=" + URLEncoder.encode("6nruJnM4", "UTF-8"));
+            Data += ("&Recipient=" + URLEncoder.encode(telNo + "", "UTF-8"));
+            Data += ("&Message=" + URLEncoder.encode(smsMessage, "UTF-8"));
+
+            int Result = -1;
+            URL Address = new URL(RequestURL);
+
+            HttpURLConnection Connection = (HttpURLConnection) Address.openConnection();
+            Connection.setRequestMethod("POST");
+            Connection.setDoInput(true);
+            Connection.setDoOutput(true);
+
+            DataOutputStream Output;
+            Output = new DataOutputStream(Connection.getOutputStream());
+            Output.writeBytes(Data);
+            Output.flush();
+            Output.close();
             return true;
         } catch (Exception ex) {
             return false;
