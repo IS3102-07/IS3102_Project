e607452119847e587b347172832816790b9edf1c
Lee Yuan Guang <leeyuanguang.lyg@gmail.com>
2014-10-16 01:39:48 +0800

    login staff logging

--- IS3102_Project-ejb/src/java/CommonInfrastructure/AccountManagement/AccountManagementWebService.java
+++ IS3102_Project-ejb/src/java/CommonInfrastructure/AccountManagement/AccountManagementWebService.java
@@ -1,7 +1,12 @@
 package CommonInfrastructure.AccountManagement;
 
+import Config.Config;
 import EntityManager.RoleEntity;
 import EntityManager.StaffEntity;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Date;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -18,18 +23,30 @@ public class AccountManagementWebService {
 
     @WebMethod
     public Long posLoginStaff(@WebParam(name = "email") String email, @WebParam(name = "password") String password) {
-        StaffEntity staffEntity = AccountManagementBeanLocal.loginStaff(email, password);
-        if (staffEntity == null) {
+        Long staffID = null;
+        try {
+            StaffEntity staffEntity = AccountManagementBeanLocal.loginStaff(email, password);
+            if (staffEntity == null) {
+                return null;
+            }
+            // Check roles, only admin, cashier or store manager can login into POS
+            List<RoleEntity> roles = staffEntity.getRoles();
+            for (RoleEntity role : roles) {
+                if (role.getId().equals(1L) || role.getId().equals(4L) || role.getId().equals(9L)) {
+                    staffID = staffEntity.getId();
+                    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(Config.logFilePath, true)));
+                    out.println(new Date().toString() + ";" + staffID + ";posLoginStaff();" + staffID + ";");
+                    out.close();
+                    return staffID;
+                }
+            }
             return null;
-        }
-        // Check roles, only admin, cashier or store manager can login into POS
-        List<RoleEntity> roles = staffEntity.getRoles();
-        for (RoleEntity role : roles) {
-            if (role.getId().equals(1L) || role.getId().equals(4L) || role.getId().equals(9L)) {
-                Long staffID = staffEntity.getId();
+        } catch (Exception ex) {
+            if (staffID != null) {
                 return staffID;
+            } else {
+                return null;
             }
         }
-        return null;
     }
 }
