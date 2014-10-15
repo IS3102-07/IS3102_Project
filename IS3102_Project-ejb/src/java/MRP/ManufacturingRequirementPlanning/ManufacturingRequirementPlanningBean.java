7105a5bd36bdc8666baf799a7adc93d5ca263381
Bo-Yee-Woods <boyeewoods@gmail.com>
2014-10-15 11:17:28 +0800

    calendar

--- IS3102_Project-ejb/src/java/MRP/ManufacturingRequirementPlanning/ManufacturingRequirementPlanningBean.java
+++ IS3102_Project-ejb/src/java/MRP/ManufacturingRequirementPlanning/ManufacturingRequirementPlanningBean.java
@@ -13,6 +13,7 @@ import EntityManager.MonthScheduleEntity;
 import EntityManager.RawMaterialEntity;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
@@ -36,7 +37,7 @@ public class ManufacturingRequirementPlanningBean implements ManufacturingRequir
             Calendar calendar = Calendar.getInstance();
             calendar.clear();
             calendar.set(Calendar.YEAR, schedule.getYear());
-            calendar.set(Calendar.MONTH, schedule.getMonth());
+            calendar.set(Calendar.MONTH, schedule.getMonth()-1);
 
             Query q1 = em.createQuery("select mps from MasterProductionScheduleEntity mps where mps.mf.id = ?1 and mps.schedule.id = ?2")
                     .setParameter(1, MfId)
@@ -82,13 +83,14 @@ public class ManufacturingRequirementPlanningBean implements ManufacturingRequir
                     }
 
                     calendar.set(Calendar.WEEK_OF_MONTH, 2);
-                    calendar.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY);
+                    calendar.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
                     System.out.println("calendar.get(Calendar.DAY_OF_MONTH) week2 :" + calendar.get(Calendar.DAY_OF_MONTH));
                     Query query2 = em.createQuery("select mr from MaterialRequirementEntity mr where mr.mf.id = ?1 and mr.rawMaterial.SKU = ?2 and mr.schedule.id =?3 and mr.day = ?4 ")
                             .setParameter(1, mf.getId())
                             .setParameter(2, lineItem.getItem().getSKU())
                             .setParameter(3, schedule.getId())
-                            .setParameter(4, calendar.get(Calendar.DAY_OF_MONTH) - 7);
+                            .setParameter(4, calendar.get(Calendar.DAY_OF_MONTH));
+                    
                     if (query2.getResultList().isEmpty()) {
                         MaterialRequirementEntity MR2 = new MaterialRequirementEntity();
                         MR2.setMf(mf);
@@ -96,7 +98,7 @@ public class ManufacturingRequirementPlanningBean implements ManufacturingRequir
                         MR2.setRawMaterial((RawMaterialEntity) lineItem.getItem());
                         MR2.setQuantity(mps.getAmount_week2() * lineItem.getQuantity());
                         MR2.setSchedule(schedule);
-                        MR2.setDay(calendar.get(Calendar.DAY_OF_MONTH) - 7);
+                        MR2.setDay(calendar.get(Calendar.DAY_OF_MONTH));
                         em.persist(MR2);
                     } else {
                         MaterialRequirementEntity MR2 = (MaterialRequirementEntity) query2.getResultList().get(0);
@@ -105,13 +107,13 @@ public class ManufacturingRequirementPlanningBean implements ManufacturingRequir
                     }
 
                     calendar.set(Calendar.WEEK_OF_MONTH, 3);
-                    calendar.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY);
+                    calendar.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
                     System.out.println("calendar.get(Calendar.DAY_OF_MONTH) week3 :" + calendar.get(Calendar.DAY_OF_MONTH));
                     Query query3 = em.createQuery("select mr from MaterialRequirementEntity mr where mr.mf.id = ?1 and mr.rawMaterial.SKU = ?2 and mr.schedule.id =?3 and mr.day = ?4 ")
                             .setParameter(1, mf.getId())
                             .setParameter(2, lineItem.getItem().getSKU())
                             .setParameter(3, schedule.getId())
-                            .setParameter(4, calendar.get(Calendar.DAY_OF_MONTH) - 7);
+                            .setParameter(4, calendar.get(Calendar.DAY_OF_MONTH));
                     if (query3.getResultList().isEmpty()) {
                         MaterialRequirementEntity MR3 = new MaterialRequirementEntity();
                         MR3.setMf(mf);
@@ -119,7 +121,7 @@ public class ManufacturingRequirementPlanningBean implements ManufacturingRequir
                         MR3.setRawMaterial((RawMaterialEntity) lineItem.getItem());
                         MR3.setQuantity(mps.getAmount_week3() * lineItem.getQuantity());
                         MR3.setSchedule(schedule);
-                        MR3.setDay(calendar.get(Calendar.DAY_OF_MONTH) - 7);
+                        MR3.setDay(calendar.get(Calendar.DAY_OF_MONTH));
                         em.persist(MR3);
                     } else {
                         MaterialRequirementEntity MR3 = (MaterialRequirementEntity) query3.getResultList().get(0);
@@ -128,13 +130,13 @@ public class ManufacturingRequirementPlanningBean implements ManufacturingRequir
                     }
 
                     calendar.set(Calendar.WEEK_OF_MONTH, 4);
-                    calendar.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY);
+                    calendar.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
                     System.out.println("calendar.get(Calendar.DAY_OF_MONTH) week4 :" + calendar.get(Calendar.DAY_OF_MONTH));
                     Query query4 = em.createQuery("select mr from MaterialRequirementEntity mr where mr.mf.id = ?1 and mr.rawMaterial.SKU = ?2 and mr.schedule.id =?3 and mr.day = ?4 ")
                             .setParameter(1, mf.getId())
                             .setParameter(2, lineItem.getItem().getSKU())
                             .setParameter(3, schedule.getId())
-                            .setParameter(4, calendar.get(Calendar.DAY_OF_MONTH) - 7);
+                            .setParameter(4, calendar.get(Calendar.DAY_OF_MONTH));
                     if (query4.getResultList().isEmpty()) {
                         MaterialRequirementEntity MR4 = new MaterialRequirementEntity();
                         MR4.setMf(mf);
@@ -142,7 +144,7 @@ public class ManufacturingRequirementPlanningBean implements ManufacturingRequir
                         MR4.setRawMaterial((RawMaterialEntity) lineItem.getItem());
                         MR4.setQuantity(mps.getAmount_week4() * lineItem.getQuantity());
                         MR4.setSchedule(schedule);
-                        MR4.setDay(calendar.get(Calendar.DAY_OF_MONTH) - 7);
+                        MR4.setDay(calendar.get(Calendar.DAY_OF_MONTH));
                         em.persist(MR4);
                     } else {
                         MaterialRequirementEntity MR4 = (MaterialRequirementEntity) query4.getResultList().get(0);
@@ -152,13 +154,13 @@ public class ManufacturingRequirementPlanningBean implements ManufacturingRequir
 
                     if (mps.getAmount_week5() != 0) {
                         calendar.set(Calendar.WEEK_OF_MONTH, 5);
-                        calendar.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY);
+                        calendar.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
                         System.out.println("calendar.get(Calendar.DAY_OF_MONTH) week5 :" + calendar.get(Calendar.DAY_OF_MONTH));
                         Query query5 = em.createQuery("select mr from MaterialRequirementEntity mr where mr.mf.id = ?1 and mr.rawMaterial.SKU = ?2 and mr.schedule.id =?3 and mr.day = ?4 ")
                                 .setParameter(1, mf.getId())
                                 .setParameter(2, lineItem.getItem().getSKU())
                                 .setParameter(3, schedule.getId())
-                                .setParameter(4, calendar.get(Calendar.DAY_OF_MONTH) - 7);
+                                .setParameter(4, calendar.get(Calendar.DAY_OF_MONTH));
                         if (query5.getResultList().isEmpty()) {
                             MaterialRequirementEntity MR5 = new MaterialRequirementEntity();
                             MR5.setMf(mf);
@@ -166,7 +168,7 @@ public class ManufacturingRequirementPlanningBean implements ManufacturingRequir
                             MR5.setRawMaterial((RawMaterialEntity) lineItem.getItem());
                             MR5.setQuantity(mps.getAmount_week5() * lineItem.getQuantity());
                             MR5.setSchedule(schedule);
-                            MR5.setDay(calendar.get(Calendar.DAY_OF_MONTH) - 7);
+                            MR5.setDay(calendar.get(Calendar.DAY_OF_MONTH));
                             em.persist(MR5);
                         } else {
                             MaterialRequirementEntity MR5 = (MaterialRequirementEntity) query5.getResultList().get(0);
