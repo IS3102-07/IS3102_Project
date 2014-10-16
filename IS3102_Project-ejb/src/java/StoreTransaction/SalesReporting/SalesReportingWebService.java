b3bdf24857cc3a6f7b039e584fe1dc8fd69d60ca
Yuan Guang <leeyuanguang.lyg@gmail.com>
2014-10-16 18:42:10 +0800

    why no push

--- IS3102_Project-ejb/src/java/StoreTransaction/SalesReporting/SalesReportingWebService.java
+++ IS3102_Project-ejb/src/java/StoreTransaction/SalesReporting/SalesReportingWebService.java
@@ -4,16 +4,20 @@ import EntityManager.ItemEntity;
 import EntityManager.LineItemEntity;
 import HelperClasses.ReturnHelper;
 import StoreTransaction.RetailInventory.RetailInventoryBeanLocal;
+import StoreTransaction.SalesRecordingWebService_Service;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import javax.xml.ws.WebServiceRef;
 
 @WebService(serviceName = "SalesReportingWebService")
 @Stateless
 public class SalesReportingWebService {
+    @WebServiceRef(wsdlLocation = "META-INF/wsdl/localhost_8080/SalesRecordingWebService/SalesRecordingWebService.wsdl")
+    private SalesRecordingWebService_Service service;
     
     @EJB
     RetailInventoryBeanLocal inventoryBean;
@@ -28,6 +32,13 @@ public class SalesReportingWebService {
         }
         String staffPasswordString = new String(staffPasword);
         return new ReturnHelper(true, "Checkout successful.");
+    }
+
+    private StoreTransaction.ReturnHelper createSalesRecord(java.lang.String staffEmail, java.lang.String password, java.lang.Long storeID, java.lang.String posName, java.util.List<StoreTransaction.LineItemEntity> itemsPurchased, java.lang.Double amountDue, java.lang.Double amountPaid, java.lang.Double amountPaidUsingPoints, java.lang.Integer loyaltyPointsDeducted, java.lang.String memberEmail) {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        StoreTransaction.SalesRecordingWebService port = service.getSalesRecordingWebServicePort();
+        return port.createSalesRecord(staffEmail, password, storeID, posName, itemsPurchased, amountDue, amountPaid, amountPaidUsingPoints, loyaltyPointsDeducted, memberEmail);
     }
     
 }
