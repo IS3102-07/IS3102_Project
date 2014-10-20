59b0b019f5a4f5ff8c9ee49416b9b235f456eb9b
Jason Sim <simmengkiat@gmail.com>
2014-10-20 22:31:52 +0800

    Merge origin/master
    Conflicts:
    	IS3102_Project-ejb/src/java/CommonInfrastructure/AccountManagement/AccountManagementBean.java
    	IS3102_Project-ejb/src/java/EntityManager/ItemEntity.java

--- IS3102_Project-ejb/src/java/EntityManager/ItemEntity.java
+++ IS3102_Project-ejb/src/java/EntityManager/ItemEntity.java
@@ -13,9 +13,12 @@ import javax.persistence.InheritanceType;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
+@XmlRootElement
 public abstract class ItemEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
