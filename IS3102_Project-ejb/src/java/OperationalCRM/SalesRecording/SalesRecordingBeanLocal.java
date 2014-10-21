bd88c05735e47c6d1bdc6f9929006d55f8726209
Lee Yuan Guang <leeyuanguang.lyg@gmail.com>
2014-10-21 16:15:38 +0800

    receiptNo

--- IS3102_Project-ejb/src/java/OperationalCRM/SalesRecording/SalesRecordingBeanLocal.java
+++ IS3102_Project-ejb/src/java/OperationalCRM/SalesRecording/SalesRecordingBeanLocal.java
@@ -5,5 +5,5 @@ import javax.ejb.Local;
 
 @Local
 public interface SalesRecordingBeanLocal {
-    public Boolean createSalesRecord(String staffEmail, String staffPassword, Long storeID, String posName,List<String> itemsPurchasedSKU, List<Integer> itemsPurchasedQty, Double amountDue, Double amountPaid, Double amountPaidUsingPoints, Integer loyaltyPointsDeducted, String memberEmail);
+    public Boolean createSalesRecord(String staffEmail, String staffPassword, Long storeID, String posName,List<String> itemsPurchasedSKU, List<Integer> itemsPurchasedQty, Double amountDue, Double amountPaid, Double amountPaidUsingPoints, Integer loyaltyPointsDeducted, String memberEmail, String receiptNo);
 }
