2b1bd7d3d980be5bd59f2d02422899b44457ad8e
Bo-Yee-Woods <boyeewoods@gmail.com>
2014-10-22 15:34:29 +0800

    entities

--- IS3102_Project-ejb/test/JUnit_Testing/CorporateManagement_FacilityManagementBeanLocal_JUnit.java
+++ IS3102_Project-ejb/test/JUnit_Testing/CorporateManagement_FacilityManagementBeanLocal_JUnit.java
@@ -1,5 +1,11 @@
 package JUnit_Testing;
 
+import CorporateManagement.FacilityManagement.FacilityManagementBeanRemote;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -8,7 +14,11 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CorporateManagement_FacilityManagementBeanLocal_JUnit {
-
+    
+    
+    FacilityManagementBeanRemote facilityManagementBean = lookupFacilityManagementBeanRemote();
+    
+    
     public CorporateManagement_FacilityManagementBeanLocal_JUnit() {
     }
 
@@ -29,166 +39,19 @@ public class CorporateManagement_FacilityManagementBeanLocal_JUnit {
     }
 
     @Test
-    public void addRegionalOffice() {
-    }
-
-    @Test
-    public void editRegionalOffice() {
-    }
-
-    @Test
-    public void removeRegionalOffice() {
-    }
-
-    @Test
-    public void viewRegionalOffice() {
-    }
-
-    @Test
-    public void viewListOfRegionalOffice() {
-    }
-
-    @Test
     public void checkNameExistsOfRegionalOffice() {
-    }
+        String str = "Asia Pacific Regional Office";
+        Boolean result = facilityManagementBean.checkNameExistsOfRegionalOffice(str);
+        assertTrue(result);
+    }        
 
-    @Test
-    public void createManufacturingFacility() {
-    }
-
-    @Test
-    public void editManufacturingFacility() {
-    }
-
-    @Test
-    public void removeManufacturingFacility() {
-    }
-
-    @Test
-    public void viewManufacturingFacility() {
-    }
-
-    @Test
-    public void viewListOfManufacturingFacility() {
-    }
-
-    @Test
-    public void checkNameExistsOfManufacturingFacility() {
-    }
-
-    @Test
-    public void getManufacturingFacilityByName() {
-    }
-
-    @Test
-    public void getManufacturingFacilityHelper() {
-    }
-
-    @Test
-    public void getManufacturingFacilityHelperList() {
-    }
-
-    @Test
-    public void addManufacturingFacilityToRegionalOffice() {
-    }
-
-    @Test
-    public void updateManufacturingFacilityToRegionalOffice() {
-    }
-
-    @Test
-    public void createStore() {
-    }
-
-    @Test
-    public void editStore() {
-    }
-
-    @Test
-    public void viewStoreEntity() {
-    }
-
-    @Test
-    public void viewListOfStore() {
-    }
-
-    @Test
-    public void checkNameExistsOfStore() {
-    }
-
-    @Test
-    public void getStoreByName() {
-    }
-
-    @Test
-    public void getStoreByID() {
-    }
-
-    @Test
-    public void removeStore() {
-    }
-
-    @Test
-    public void addStoreToRegionalOffice() {
-    }
-
-    @Test
-    public void updateStoreToRegionalOffice() {
-    }
-
-    @Test
-    public void getStoreHelperClass() {
-    }
-
-    @Test
-    public void getStoreHelperList() {
-    }
-
-    @Test
-    public void getStoreListByRegionalOffice() {
-    }
-
-    @Test
-    public void createWarehouse() {
-    }
-
-    @Test
-    public void editWarehouse() {
-    }
-
-    @Test
-    public void deleteWarehouse() {
-    }
-
-    @Test
-    public void getWarehouseByName() {
-    }
-
-    @Test
-    public void checkNameExistsOfWarehouse() {
-    }
-
-    @Test
-    public void checkIfWarehouseContainsItem() {
-    }
-
-    @Test
-    public void getWarehouseById() {
-    }
-
-    @Test
-    public void getWarehouseList() {
-    }
-
-    @Test
-    public void getMFWarehouseList() {
-    }
-
-    @Test
-    public void getStoreWarehouseList() {
-    }
-
-    @Test
-    public void getListOfCountries() {
+    private FacilityManagementBeanRemote lookupFacilityManagementBeanRemote() {
+        try {
+            Context c = new InitialContext();
+            return (FacilityManagementBeanRemote) c.lookup("java:global/IS3102_Project/IS3102_Project-ejb/FacilityManagementBean!CorporateManagement.FacilityManagement.FacilityManagementBeanRemote");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
     }
 }
