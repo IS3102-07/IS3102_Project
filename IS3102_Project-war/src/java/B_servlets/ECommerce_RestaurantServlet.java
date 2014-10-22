a8c9efbae52624e15fd7ecc3115e6351b19393dd
Loi Liang Yang <loiliangyang@gmail.com>
2014-10-22 23:25:50 +0800

    Merge origin/master
    Conflicts:
    	IS3102_Project-ejb/src/java/AnalyticalCRM/SegmentationMarketing/SegmentationMarketingBean.java

--- IS3102_Project-war/src/java/B_servlets/ECommerce_RestaurantServlet.java
+++ IS3102_Project-war/src/java/B_servlets/ECommerce_RestaurantServlet.java
@@ -1,47 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package B_servlets;
 
+import CorporateManagement.RestaurantManagement.RestaurantManagementBeanLocal;
+import EntityManager.MenuItemEntity;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.ejb.EJB;
+import javax.servlet.http.HttpSession;
+import java.util.List;
 /**
  *
  * @author yang
  */
 public class ECommerce_RestaurantServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    @EJB
+    private RestaurantManagementBeanLocal restaurantManagementBean;
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ECommerce_RestaurantServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ECommerce_RestaurantServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        PrintWriter out = response.getWriter();
+
+        try {
+
+            HttpSession session;
+            session = request.getSession();
+            
+            List<MenuItemEntity> menuItems = restaurantManagementBean.listAllMenuItem();
+            session.setAttribute("menuItems", menuItems);
+            
+            response.sendRedirect("B/restaurant.jsp");
+            
+        } catch (Exception ex) {
+            out.println("\n\n " + ex.getMessage());
         }
     }
 
