16f86ab904c3a26432b722f92a1536d0faa9872c
Neo Wei <neowei03@gmail.com>
2014-10-23 21:36:22 +0800

    junit for io log

--- IS3102_Project-ejb/test/JUnit_Testing/CorporateManagement_ItemManagementBeanRemote_JUnit.java
+++ IS3102_Project-ejb/test/JUnit_Testing/CorporateManagement_ItemManagementBeanRemote_JUnit.java
@@ -22,7 +22,11 @@ import org.junit.AfterClass;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 
 public class CorporateManagement_ItemManagementBeanRemote_JUnit {
 
