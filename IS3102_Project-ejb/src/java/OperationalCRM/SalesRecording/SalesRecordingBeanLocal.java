8085052347b73817b02efa6cdfb4cd3c20902085
Yuan Guang <leeyuanguang.lyg@gmail.com>
2014-10-25 17:10:38 +0800

    tat kitchen!

--- IS3102_Project-ejb/src/java/OperationalCRM/SalesRecording/SalesRecordingBeanLocal.java
+++ IS3102_Project-ejb/src/java/OperationalCRM/SalesRecording/SalesRecordingBeanLocal.java
@@ -6,4 +6,6 @@ import javax.ejb.Local;
 @Local
 public interface SalesRecordingBeanLocal {
     public Boolean createSalesRecord(String staffEmail, String staffPassword, Long storeID, String posName,List<String> itemsPurchasedSKU, List<Integer> itemsPurchasedQty, Double amountDue, Double amountPaid, Double amountPaidUsingPoints, Integer loyaltyPointsDeducted, String memberEmail, String receiptNo);
+    public Boolean createSyncWithPhoneRequest(String qrCode);
+    public String getSyncWithPhoneStatus(String qrCode);
 }
