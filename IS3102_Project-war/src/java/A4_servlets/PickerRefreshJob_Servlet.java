a1cd5963661b762e77950f4151a1c7c0f9d599d6
Neo Wei <neowei03@gmail.com>
2014-10-31 18:46:05 +0800

    changing to picker id to entity

--- IS3102_Project-war/src/java/A4_servlets/PickerRefreshJob_Servlet.java
+++ IS3102_Project-war/src/java/A4_servlets/PickerRefreshJob_Servlet.java
@@ -1,28 +1,36 @@
 package A4_servlets;
 
+import EntityManager.PickRequestEntity;
+import EntityManager.PickerEntity;
+import OperationalCRM.CustomerService.CustomerServiceBeanLocal;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class PickerRefreshJob_Servlet extends HttpServlet {
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @EJB
+    CustomerServiceBeanLocal customerServiceBeanLocal;
 
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet PickerRefreshJob_Servlet</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet PickerRefreshJob_Servlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        PrintWriter out = response.getWriter();
+        try {
+            HttpSession session;
+            session = request.getSession();
+
+            PickerEntity picker = (PickerEntity) (session.getAttribute("picker"));
+
+            //PickRequestEntity pickRequest = customerServiceBeanLocal.getPickRequests(picker.getId());
+            //session.setAttribute("PickRequestEntity", pickRequest);
+
+        } catch (Exception ex) {
+            out.println(ex);
         }
     }
 
