2d2d70f57678c730a9c34b7f820cb310fdf91f64
Neo Wei <neowei03@gmail.com>
2014-11-01 14:22:33 +0800

    promotional web service

--- IS3102_Project-ejb/src/java/OperationalCRM/PromotionalSales/PromotionalSalesWebService.java
+++ IS3102_Project-ejb/src/java/OperationalCRM/PromotionalSales/PromotionalSalesWebService.java
@@ -1,11 +1,14 @@
 package OperationalCRM.PromotionalSales;
 
+import EntityManager.StoreEntity;
 import java.util.Date;
 import javax.ejb.EJB;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @WebService(serviceName = "PromotionalSalesWebService")
 @Stateless()
@@ -14,8 +17,19 @@ public class PromotionalSalesWebService {
     @EJB
     PromotionalSalesBeanLocal psbl;
     
+    @PersistenceContext(unitName = "IS3102_Project-ejbPU")
+    private EntityManager em;
+    
     @WebMethod
-    public Double getPromotionRate(@WebParam(name = "SKU")String sku, @WebParam(name = "countryID")Long countryId, @WebParam(name = "date")Date date){
-        return psbl.getPromotionRate(sku, countryId, date);
+    public Double getPromotionRate(@WebParam(name = "SKU")String sku, @WebParam(name = "storeID")Long storeID, @WebParam(name = "date")Date date){
+        try {
+            StoreEntity storeEntity = em.getReference(StoreEntity.class, storeID);
+            Long countryId = storeEntity.getCountry().getId();
+            return psbl.getPromotionRate(sku, countryId, date);
+        } catch (Exception ex) {
+            System.out.println("getPromotionRate(): Error");
+            ex.printStackTrace();
+            return 0.0;
+        }
     }
 }
