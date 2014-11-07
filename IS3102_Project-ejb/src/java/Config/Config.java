cef83f9a4270dd73056d37d322a2427a90fbfd5d
Neo Wei <neowei03@gmail.com>
2014-11-07 15:36:17 +0800

    huat

--- IS3102_Project-ejb/src/java/Config/Config.java
+++ IS3102_Project-ejb/src/java/Config/Config.java
@@ -1,7 +1,8 @@
 package Config;
 
 public class Config {
-    //This will be in %APPDATA%\Roaming\NetBeans\8.0\config\GF_4.0\domain1\config
+    //This will be in %APPDATA%\Roaming\NetBeans\8.0\config\GF_4.0\domain1\config for windows 8 
+    //C:\Program Files\glassfish-4.1\glassfish\domains\domain1\config for windows 7
     public static String logFilePath = ".\\IslandFurnitureSystemLog.txt";
     
     //Volume >= to the value below will be redirected to collection area for the checkout process
